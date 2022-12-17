7a67877201408514178a8cadb97f99e6a3f42ccd
Bruno Voisin <bruno@voisin.dev>
2022-12-18 00:50:47 +0100

    Day 2, puzzle 2

--- day2/puzzles.py
+++ day2/puzzles.py
@@ -24,8 +24,14 @@ class Round:
         outcome: Optional[Outcome] = None,
     ):
         self.opponent = opponent
-        self.me = me
-        self.outcome = compute_outcome(opponent, me)
+        if me is None:
+            self.me = compute_me(opponent, outcome)
+        else:
+            self.me = me
+        if outcome is None:
+            self.outcome = compute_outcome(opponent, me)
+        else:
+            self.outcome = outcome
 
 
 def compute_outcome(opponent, me):
@@ -39,6 +45,16 @@ def compute_outcome(opponent, me):
     raise Exception("unexpected case")
 
 
+def compute_me(opponent, outcome):
+    if outcome == LOST:
+        return Shape((opponent.score - 1 - 1) % 3 + 1)
+    if outcome == DRAW:
+        return opponent
+    if outcome == WON:
+        return Shape((opponent.score + 1 - 1) % 3 + 1)
+    raise Exception("unexpected case")
+
+
 ROCK = Shape(1)
 PAPER = Shape(2)
 SCISSOR = Shape(3)
@@ -46,31 +62,45 @@ LOST = Outcome(0)
 DRAW = Outcome(3)
 WON = Outcome(6)
 
-CODE_MAP = {
+OPPONENT_CODE_MAP = {
     "A": ROCK,
     "B": PAPER,
     "C": SCISSOR,
+}
+MY_CODE_MAP = {
     "X": ROCK,
     "Y": PAPER,
     "Z": SCISSOR,
 }
+OUTCOME_CODE_MAP = {
+    "X": LOST,
+    "Y": DRAW,
+    "Z": WON,
+}
 
 
 def puzzle1(lines):
-    return sum(compute_score(parse_line(line)) for line in lines)
+    return sum(compute_score(parse_puzzle1_line(line)) for line in lines)
 
 
 def compute_score(round_: Round) -> int:
     return round_.outcome.score + round_.me.score
 
 
-def parse_line(line) -> Round:
+def parse_puzzle1_line(line) -> Round:
     opponent_code, my_code = line.split()
-    return Round(CODE_MAP[opponent_code], CODE_MAP[my_code])
+    return Round(OPPONENT_CODE_MAP[opponent_code], MY_CODE_MAP[my_code])
 
 
 def puzzle2(lines):
-    pass
+    return sum(compute_score(parse_puzzle2_line(line)) for line in lines)
+
+
+def parse_puzzle2_line(line) -> Round:
+    opponent_code, outcome_code = line.split()
+    return Round(
+        OPPONENT_CODE_MAP[opponent_code], outcome=OUTCOME_CODE_MAP[outcome_code]
+    )
 
 
 def read_input_lines():
@@ -79,4 +109,4 @@ def read_input_lines():
 
 
 if __name__ == "__main__":
-    print(puzzle1(read_input_lines()))
+    print(puzzle2(read_input_lines()))
--- day2/test_puzzles.py
+++ day2/test_puzzles.py
@@ -1,10 +1,11 @@
+from puzzles import compute_me
+from puzzles import compute_outcome
 from puzzles import DRAW
 from puzzles import LOST
 from puzzles import PAPER
 from puzzles import puzzle1
 from puzzles import puzzle2
 from puzzles import ROCK
-from puzzles import Round
 from puzzles import SCISSOR
 from puzzles import WON
 
@@ -14,16 +15,23 @@ with open("example.txt") as f:
 
 
 def test_round_outcome():
-    assert Round(ROCK, PAPER).outcome == WON
-    assert Round(PAPER, ROCK).outcome == LOST
-    assert Round(SCISSOR, SCISSOR).outcome == DRAW
-    assert Round(SCISSOR, ROCK).outcome == WON
-    assert Round(ROCK, SCISSOR).outcome == LOST
+    assert compute_outcome(ROCK, PAPER) == WON
+    assert compute_outcome(PAPER, ROCK) == LOST
+    assert compute_outcome(SCISSOR, SCISSOR) == DRAW
+    assert compute_outcome(SCISSOR, ROCK) == WON
+    assert compute_outcome(ROCK, SCISSOR) == LOST
 
 
 def test_puzzle1():
     assert puzzle1(LINES) == 15
 
 
+def test_compute_me():
+    assert compute_me(ROCK, DRAW) == ROCK
+    assert compute_me(PAPER, LOST) == ROCK
+    assert compute_me(SCISSOR, WON) == ROCK
+    assert compute_me(SCISSOR, LOST) == PAPER
+
+
 def test_puzzle2():
-    assert puzzle2(LINES) is None
+    assert puzzle2(LINES) == 12
